ade1385d862bd504f9de97c2c4449c36437e34a8
Sanjeev Kumar <Sanjeev.Kumar@intimetec.com>
2019-06-28 15:26:24 +0530

    Unit test case file commit for hashing

---
+++ Ruby/spec/khash_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+# This suite of tests focuses on the Encryption and the state of
+# the client when initialized. The usage (kount/utils/khash) aspects
+# of the client are covered in the client_spec.rb test suite.
+
+describe "Test: Khash encode method do" do
+
+  context "Hash Technique for encode text" do
+
+    describe "Test the HashPaymentToken Method for Credit Card and others expect GIFT and CHEK" do
+      it 'Test the encode method for the value Ruby' do
+        expect(Kount::Khash.HashPaymentToken('4111111111111111', '')).to eq('41111193FBKM1EIVIXVP')
+      end
+    end
+    
+    describe "Test the HashGiftCard Method for Gift Card type" do
+      it 'Test the HashGiftCard Method for Gift Card type' do
+        expect(Kount::Khash.HashGiftCard('4111111111111111', '', '900431')).to eq('90043193FBKM1EIVIXVP')
+      end
+    end
+
+    describe "Test the HashCheckPayment Method" do
+      it 'Test the HashGiftCard Method for Gift Card type' do
+        expect(Kount::Khash.HashCheckPayment('01234567811111111111111', '')).to eq('012345440GD428TF29K0')
+      end
+    end
+  end
+
+end
+
