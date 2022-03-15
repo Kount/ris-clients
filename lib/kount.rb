121a6f87c7ba062fa02a2c9b3df0613481673697
CORP\Abhishek.Raj <abhishek.raj@kount.com>
2022-03-15 12:36:06 +0530

    adding required space

--- lib/kount.rb
+++ lib/kount.rb
@@ -20,7 +20,7 @@ module Kount
   #    }
   # @param options Hash
   def new(options = {})
-    raise ArgumentError,'Config options required' if options.empty?
+    raise ArgumentError, 'Config options required' if options.empty?
     Client.new(options)
   end
 
